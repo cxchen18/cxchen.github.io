b66436fd3fe17ab9cfba25e148cc1deafbf0662f
Claire Chen <clairechen@Claires-MacBook-Pro-2.local>
2024-04-15 22:07:47 +1000

    added file structure

---
+++ workbook/main.js
@@ -0,0 +1,10 @@
+// on load audio player from w3schools
+const audio = new Audio("workbook/Assets/choo-choo-sound.mp3");
+
+audio.play();
+
+//window.addEventListener('load', function() {
+    var audio = document.getElementById('audio');
+    audio.play();
+ // });
+
